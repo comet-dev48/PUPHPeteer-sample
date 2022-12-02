c8c5f6ea075f4a6b3fc428749c887c0a021f8992
jhonalic <jhonbrownlove@gmail.com>
2022-12-02 09:16:56 +0900

    fix variable issue

--- test.php
+++ test.php
@@ -1,24 +1,26 @@
 <?php
 
-require_once  './vendor/autoload.php' ;
+require_once './vendor/autoload.php';
 
 use Nesk\Puphpeteer\Puppeteer;
 use Nesk\Rialto\Data\JsFunction;
 
 $puppeteer = new Puppeteer;
-$browser   = $puppeteer->launch();
+$browser = $puppeteer->launch();
 
-$ page  =  $ browser -> newPage () ;
- $ page -> goto ( 'https://github.com/kojirock5260?tab=repositories' ) ;
+$page = $browser->newPage();
+$page->goto('https://github.com/kojirock5260?tab=repositories');
 
-$ repositories  =  [] ;
- foreach  ( $ page -> querySelectorAll ( '.wb-break-all' )  as  $ element )  { 
-    $ repositories []  =  trim ( $ element -> querySelectorEval ( 'a' ,
-        \Nesk\Rialto\Data\JsFunction :: createWithParameters ([ 'element' ]) -> body ( 'return element.innerHTML' ) 
-    )) ;
- } 
-$ browser -> close () ;
- var_dump ( $ repositories ) ;
+$repositories = [];
+foreach ($page->querySelectorAll('.wb-break-all') as $element) {
+    $repositories[] = trim($element->querySelectorEval(
+        'a',
+            \Nesk\Rialto\Data\JsFunction::createWithParameters(['element'])->body('return element.innerHTML')
+    )
+    );
+}
+$browser->close();
+var_dump($repositories);
 
- #note
- #https://kojirooooocks.hatenablog.com/entry/2020/09/30/100000
+#note
+#https://kojirooooocks.hatenablog.com/entry/2020/09/30/100000
