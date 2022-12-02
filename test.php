ea08703fea11068a6fc36a8c49bcddf403228a89
unknown <jhonbrownlove@gmail.com>
2022-12-02 09:06:47 +0900

    update test content

--- test.php
+++ test.php
@@ -6,12 +6,19 @@ use Nesk\Puphpeteer\Puppeteer;
 use Nesk\Rialto\Data\JsFunction;
 
 $puppeteer = new Puppeteer;
-
 $browser   = $puppeteer->launch();
 
-$page = $browser->newPage();
-$page->goto('https://kojirooooocks.hatenablog.com/');
-$page->type('.search-module-input', 'laravel');
-$page->click('.search-module-button');
-$page->screenshot(['path' => 'example2.png']);
-$browser->close();
+$ page  =  $ browser -> newPage () ;
+ $ page -> goto ( 'https://github.com/kojirock5260?tab=repositories' ) ;
+
+$ repositories  =  [] ;
+ foreach  ( $ page -> querySelectorAll ( '.wb-break-all' )  as  $ element )  { 
+    $ repositories []  =  trim ( $ element -> querySelectorEval ( 'a' ,
+        \Nesk\Rialto\Data\JsFunction :: createWithParameters ([ 'element' ]) -> body ( 'return element.innerHTML' ) 
+    )) ;
+ } 
+$ browser -> close () ;
+ var_dump ( $ repositories ) ;
+
+ #note
+ #https://kojirooooocks.hatenablog.com/entry/2020/09/30/100000
